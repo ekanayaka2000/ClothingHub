2200eaf3b0f7fa6a39da4cf75790605cc168bf0b
Pramod Ekanayaka <128894685+ekanayaka2000@users.noreply.github.com>
2025-06-14 23:21:49 +0530

    update

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,5 +1,47 @@
-function ShoppingListing(){
-    return <div>Shopping view Listing</div>;
+import ProductFilter from "@/components/shopping-view/filter";
+import { Button } from "@/components/ui/button";
+import { DropdownMenuTrigger, DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem } from "@/components/ui/dropdown-menu";
+import { sortOptions } from "@/config";
+import { ArrowUpDownIcon } from "lucide-react";
+
+
+function ShoppingListing() {
+    return <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
+        <ProductFilter />
+        <div className="bg-background w-full rounded-lg shadow-sm">
+            <div className="p-4 border-b flex items-center justify-between">
+                <h2 className="text-lg font-extrabold">All Products</h2>
+                <div className="flex items-center gap-3">
+                    <span className="text-muted-foreground">
+                        Products
+                    </span>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline"
+                                size="sm"
+                                className="flex items-center gap-1">
+                                <ArrowUpDownIcon className="h-4 w-4" />
+                                <span>
+                                    Sort by
+                                </span>
+                            </Button>
+
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end" className="w-[200px]">
+                            <DropdownMenuRadioGroup>
+                            {
+                                sortOptions.map(sortItem=><DropdownMenuRadioItem  
+                                key={sortItem.id}>{sortItem.label}
+                                    
+                                </DropdownMenuRadioItem>)
+                            }
+                            </DropdownMenuRadioGroup>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            </div>
+        </div>
+    </div>;
 }
 
 export default ShoppingListing;
