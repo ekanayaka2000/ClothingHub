64ec650c80f4e06b35540aca467c18d2c786b8ec
Pramod Ekanayaka <128894685+ekanayaka2000@users.noreply.github.com>
2025-06-18 22:40:08 +0530

    update

--- client/src/pages/shopping-view/checkout.jsx
+++ client/src/pages/shopping-view/checkout.jsx
@@ -82,7 +82,7 @@ function ShoppingCheckout() {
     };
 
     dispatch(createNewOrder(orderData)).then((data) => {
-      console.log(data, "sangam");
+      console.log(data, "Pramod");
       if (data?.payload?.success) {
         setIsPaymemntStart(true);
       } else {
