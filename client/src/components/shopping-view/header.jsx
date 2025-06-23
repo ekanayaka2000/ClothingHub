48532e4ff61119ac5827c961562c8bbe7c5789d2
Pramod Ekanayaka <128894685+ekanayaka2000@users.noreply.github.com>
2025-06-23 20:36:24 +0530

    up new

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -23,6 +23,7 @@ import UserCartWrapper from "./cart-wrapper";
 import { useEffect, useState } from "react";
 import { fetchCartItems } from "@/store/shop/cart-slice";
 import { Label } from "../ui/label";
+import ClothingHubLogo from "../../assets/ClothingHub.jpg";
 
 function MenuItems() {
     const navigate = useNavigate();
@@ -138,8 +139,8 @@ function ShoppingHeader() {
     return (
         <header className="sticky top-0 z-40 w-full border-b bg-background">
             <div className="flex h-16 items-center justify-between px-4 md:px-6">
-                <Link to="/shop/home" className="flex items-center gap-2">
-                    <HousePlug className="h-6 w-6" />
+                <Link to="/shop/home" className="flex items-center gap-3">
+                    <img src={ClothingHubLogo} alt="ClothingHub Logo" className="h-16 w-16 object-contain" />
                     <span className="font-bold">ClothingHub</span>
                 </Link>
                 <Sheet>
